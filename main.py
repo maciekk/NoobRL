0369e754f0eced2764fce57afb20c5f8a1ce71a0
maciekk <mkalisiak@gmail.com>
2022-08-01 10:21:42 -0400

    Initial version.

---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import tcod
+
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
+from procgen import generate_dungeon
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 40
+
+    map_width = 80
+    map_height = 35
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    tileset = tcod.tileset.load_tilesheet(
+        #"dejavu10x10_gs_tc.png", 32, 8,
+        #"Bedstead-20-df.png", 16, 16,
+        #"Curses_1920x900.png", 16, 16,
+        "Curses_24pt_cleartype_ThomModifications.png", 16, 16,
+        #tcod.tileset.CHARMAP_TCOD
+        tcod.tileset.CHARMAP_CP437
+    )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            engine.render(console=root_console, context=context)
+
+            events = tcod.event.wait()
+
+            engine.handle_events(events)
+
+
+if __name__ == "__main__":
+    main()
